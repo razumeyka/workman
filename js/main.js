5627b685aece715a432b2021bc625169bce47638
razumeyka <33911265+razumeyka@users.noreply.github.com>
2018-07-31 12:47:24 +0300

    license js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel .next,.carousel .prev').click(function(){
+        console.log('click carousel');
+        var countslides=jQuery('.review_container').children().children().length-Math.round(parseFloat(jQuery('.review_wrapper').css('width'))/parseFloat(jQuery('.one_doc').css('width')));
+        var slide=$(this).closest('.carousel').data('slide');
+        console.log(countslides);
+        if($(this).hasClass('next')){
+            slide++;
+            if(slide>countslides)slide=0;
+        }
+        if($(this).hasClass('prev')){
+            slide--;
+            if(slide<0)slide=countslides;
+        }
+        
+        $(this).closest('.carousel').find('.long').css('margin-left','-'+(slide*25)+'%');
+		$(this).closest('.carousel').data('slide',slide);
+    })
+})
